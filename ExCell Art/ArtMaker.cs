7c1b12d5cdb645f31769ab1fe6142d7af9b4f441
JamesMatchett <30292929+JamesMatchett@users.noreply.github.com>
2018-10-01 20:20:06 +0100

    This little trick grealy increases the effeciency of the multihreaded method

--- ExCell Art/ArtMaker.cs
+++ ExCell Art/ArtMaker.cs
@@ -11,13 +11,12 @@ using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExCell_Art
 {
-   public class ArtMaker
+    public class ArtMaker
     {
-      public BackgroundWorker bw = new BackgroundWorker();
+        public BackgroundWorker bw = new BackgroundWorker();
         string ImagePath;
         string OutputPath;
         int PercComplete;
-
         bool Quitting = false;
 
         public Excel.Application xlApp = new Excel.Application();
@@ -42,7 +41,6 @@ namespace ExCell_Art
             Quitting = true;
             bw.CancelAsync();
             bw.Dispose();
-            
         }
 
         public void bw_makeArt(object sender, EventArgs e)
@@ -50,15 +48,15 @@ namespace ExCell_Art
             //read input image
             Bitmap bm = new Bitmap(ImagePath);
             //create a new excel document
-           
-            
-            
+
             Excel.Workbook xlWorkbook = xlApp.Workbooks.Add();
             Excel._Worksheet xlWorksheet = (Excel.Worksheet)xlWorkbook.Worksheets.get_Item(1);
             xlWorksheet.Unprotect();
             xlWorksheet.StandardWidth = 20 / 7.25;
-            
+
             Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            ColorSetter cs = new ColorSetter(xlRange);
 
             //assign it here rather than in the for loop to avoid multithreading calamities
             var Width = bm.Width;
@@ -66,9 +64,11 @@ namespace ExCell_Art
 
             decimal totalPixels = (bm.Width - 1) * (bm.Height - 1);
             decimal pixelCounter = 0;
+
             //i = across, j = up, image coordinates start from bottom left corner whereas excel starts from top left
-            Parallel.For(0, Height - 1, (j,loopState) =>
+            Parallel.For(0, Height - 1, (j, loopState) =>
             {
+
                 var bmClone_ = bm.Clone();
                 Bitmap bmClone = ((Bitmap)(bmClone_));
                 for (int i = 0; i < Width - 1; i++)
@@ -78,11 +78,12 @@ namespace ExCell_Art
                     {
                         try
                         {
-                            xlRange.Cells[j + 1, i + 1].Interior.Color = System.Drawing.ColorTranslator.ToOle(bmClone.GetPixel(i, j));
+                            cs.setColor(i, j, ref bmClone);
                             pixelCounter++;
-                        } catch
+                        }
+                        catch
                         {
-                            
+
                         }
                     }
                     else
@@ -92,30 +93,50 @@ namespace ExCell_Art
                         break;
                     }
                 }
-
                 if (Quitting)
                 {
                     loopState.Stop();
                 }
-
-                decimal progress = ((pixelCounter / totalPixels)*100);
+                string msg = pixelCounter + "/" + totalPixels;
+                Debug.WriteLine(msg);
+                decimal progress = ((pixelCounter / totalPixels) * 100);
                 int progressInt = Convert.ToInt32(progress);
                 bw.ReportProgress(progressInt);
             });
 
-           
             if (!Quitting)
             {
                 xlWorkbook.SaveAs(OutputPath);
                 bw.ReportProgress(101);
             }
-            
+
+            xlRange = cs.Finish();
             xlWorkbook.Close(0);
             xlApp.Quit();
             System.Runtime.InteropServices.Marshal.ReleaseComObject(xlWorkbook);
             System.Runtime.InteropServices.Marshal.ReleaseComObject(xlApp);
-
         }
 
+
+
+        private class ColorSetter
+        {
+            Excel.Range _xlRange;
+            public ColorSetter(Excel.Range xlRange)
+            {
+                _xlRange = xlRange;
+            }
+
+            public void setColor(int i, int j, ref Bitmap bmClone)
+            {
+                var x = _xlRange.Cells[j + 1, i + 1];
+                x.Interior.Color = ColorTranslator.ToOle(bmClone.GetPixel(i, j));
+            }
+
+            public Excel.Range Finish()
+            {
+                return _xlRange;
+            }
+        }
     }
 }
